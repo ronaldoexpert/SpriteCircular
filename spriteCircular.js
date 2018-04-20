793d16cc053c9cfe2fd4b863826fac60e3e1e970
Ronaldo Silveira <contato@expertme.com.br>
2018-04-20 20:49:10 -0300

    Criação do movimento e da colisão

--- spriteCircular.js
+++ spriteCircular.js
@@ -3,23 +3,32 @@ function Sprite() {
   this.y = 100;
   this.w = 20;
   this.h = 20;
+  this.vx = 0;
+  this.vy = 0;
+  this.raio = 20;
+  this.cor = "blue";
 }
 
 Sprite.prototype.desenhar = function (ctx) {
   ctx.fillStyle = this.cor;
   ctx.strokeStyle = 'white';
-
-  ctx.lineWidth = 3;
+  ctx.lineWidth = 2;
   ctx.save();
   ctx.translate(this.x, this.y);
-  ctx.rotate(this.ang*180/Math.PI);
-  ctx.fillRect(-this.w/2, -this.h/2, this.w, this.h);
-  ctx.strokeRect(-this.w/2, -this.h/2, this.w, this.h);
+  ctx.save();
+  ctx.beginPath();
+  ctx.arc(0, 0, this.raio, 0, Math.PI*2, false);
+  ctx.fillStyle = this.cor;
+  ctx.fill();
+  ctx.fill();
+  ctx.stroke();
+  ctx.restore();
   ctx.restore();
 }
 
 Sprite.prototype.mover = function (dt) {
-
+  this.x = this.x + this.vx * dt;
+  this.y = this.y + this.vy * dt;
 }
 
 Sprite.prototype.perseguir = function (alvo){
@@ -28,10 +37,29 @@ Sprite.prototype.perseguir = function (alvo){
 }
 
 Sprite.prototype.impoeLimites = function(x, y, w, h){
-
-
+  if(this.x - this.raio < x ) {
+      this.x = x + this.raio;
+      this.vx = 0;
+    }
+    if(this.x + this.raio > x + w) {
+      this.x = x + w - this.raio;
+      this.vx = 0;
+    }
+    if(this.y - this.raio < y ) {
+      this.y = y + this.raio;
+      this.vy = 0;
+    }
+    if(this.y + this.raio > y + h) {
+      this.y = y + h - this.raio;
+      this.vy = 0;
+    }
 };
 
 Sprite.prototype.colidiuCom = function (alvo) {
-
+  var dist = Math.pow(Math.pow(alvo.x - this.x,2)+Math.pow(alvo.y - this.y,2),0.5);
+  if (dist < this.raio + alvo.raio){
+    return true;
+  }else {
+    return false;
+  }
 };
